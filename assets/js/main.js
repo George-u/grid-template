1987fd57c84d9c7dc244c0c50fb2dea2c174a665
George-u <yuriy.motyl@gmail.com>
2021-09-13 12:57:23 +0300

    updated path for jquery.min.js file, it fixed the issue with menu list display on mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $('.header-navbar__hamburger').on('click', function() {
-        $(this).toggleClass('header-navbar__hamburger--active');
-        $('.header-navbar__list').toggleClass('header-navbar__list--active');
+      $(this).toggleClass('header-navbar__hamburger--active');
+      $('.header-navbar__list').stop(true, true).slideToggle(500);
     });
-});
+  });
